6bb5bd74763130480871664923e58b6b7c0f4018
top <dks2922@gmail.com>
2023-02-15 21:41:49 +0900

    feat : 최근 토큰값 조회 query dsl 추가

--- src/main/java/com/pintalk/openbank/repository/TokenRepository.java
+++ src/main/java/com/pintalk/openbank/repository/TokenRepository.java
@@ -2,9 +2,13 @@ package com.pintalk.openbank.repository;
 
 import com.pintalk.openbank.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
+
+    @Query("select a.access_token from Token a where a.no = (select max(u.no) from Token u)")
+    String getMaxAccessToken();
 }
