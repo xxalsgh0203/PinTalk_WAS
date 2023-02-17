fa963cfbc40fce0f2c456f1bae246223b0f2c22f
top <dks2922@gmail.com>
2023-02-17 19:17:29 +0900

    feat : 오픈뱅킹 사용자인증 method 추가

--- src/main/java/com/pintalk/openbank/controller/OpenBankController.java
+++ src/main/java/com/pintalk/openbank/controller/OpenBankController.java
@@ -3,6 +3,7 @@ package com.pintalk.openbank.controller;
 import com.pintalk.openbank.service.OpenBankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,7 @@ public class OpenBankController {
     @Autowired
     OpenBankService openBankService;
 
-    @RequestMapping(path = "/openBank/authorize")
+    @RequestMapping(path = "/openBank/authorize", method = RequestMethod.GET)
     public String authorize() {
         return openBankService.authorize();
     }
